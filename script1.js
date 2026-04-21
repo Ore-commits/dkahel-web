2b5bad688436e35aa8eeba92f17a0f83e9504e3c
Ore-commits <kahelore45@gmail.com>
2026-04-21 08:53:12 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -5,3 +5,14 @@
 
 
 console.log("Productos D'KAHEL cargados 🍰");
+function enviarWhatsApp(mensaje) {
+    let hora = new Date().getHours();
+
+    if (hora < 11 || hora >= 22) {
+        alert("⛔ Nuestro horario es de 11:00 AM a 10:00 PM");
+        return;
+    }
+
+    let url = "https://wa.me/51919495679?text=" + encodeURIComponent(mensaje);
+    window.open(url, "_blank");
+}
