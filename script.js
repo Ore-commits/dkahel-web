e649ffcdf4234a904bb9149fbfe42b2499873dc8
Ore-commits <kahelore45@gmail.com>
2026-04-20 20:04:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+// Mensaje de bienvenida sin tocar diseño
+window.onload = function() {
+    console.log("🍰 D'KAHEL cargado correctamente");
+
+    // mensaje opcional
+    alert("Bienvenido a D'KAHEL 🍰 Postres hechos con amor");
+};
+
+
