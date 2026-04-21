2ce1cdc746d335f543a46fe03fb7af4a2c4e070a
Ore-commits <kahelore45@gmail.com>
2026-04-21 08:51:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,31 @@ window.onload = function() {
     // mensaje opcional
     alert("Bienvenido a D'KAHEL 🍰 Postres hechos con amor");
 };
+function enviarWhatsApp() {
+    let hora = new Date().getHours();
 
+    if (hora >= 22 || hora < 11) {
+        alert("⛔ No estamos atendiendo ahora. Horario: 11:00 AM - 10:00 PM");
+        return;
+    }
+
+    window.open("https://wa.me/51999016622", "_blank");
+}
+
+function actualizarEstado() {
+    let hora = new Date().getHours();
+    let estado = document.getElementById("estado-tienda");
+
+    if (hora >= 11 && hora < 22) {
+        estado.textContent = "🟢 ABIERTO";
+        estado.classList.add("abierto");
+        estado.classList.remove("cerrado");
+    } else {
+        estado.textContent = "🔴 CERRADO";
+        estado.classList.add("cerrado");
+        estado.classList.remove("abierto");
+    }
+}
+
+actualizarEstado();
 
